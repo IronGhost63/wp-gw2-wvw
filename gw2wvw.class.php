482b3fa203abb8c00fec3aae1f1927188ed4e68c
Jirayu Yingthawornsuk <me@jirayu.in.th>
2017-08-09 20:25:39 +0700

    Shortcode to display wvw score

--- gw2wvw.class.php
+++ gw2wvw.class.php
@@ -1,13 +1,11 @@
 <?php
-function get_transient($name){
-	return false;
-}
-
 class Gw2wvw {
 	public $worlds;
 	public $current_world;
 	public $match_info;
+	public $home_world;
 	public $home_color;
+	public $linked;
 
 	function __construct($options = array()){
 		if(empty($option['current_world'])){
@@ -39,33 +37,83 @@ class Gw2wvw {
 					$gw2worlds[$single->id] = $single->name;
 				}
 			}
-			//set_transient( 'gw2_worlds', $gw2worlds );
+			set_transient( 'gw2_worlds', $gw2worlds );
 		}
 
 		return $gw2worlds;
 	}
+
+	//public function get_linked_server
 
 	public function get_match_info($world_id = ""){
 		if(empty($world_id)){
 			$world_id = $this->current_world;
 		}
 
-		$data = $this->get_gw_api( "https://api.guildwars2.com/v2/wvw/matches?world=" . $world_id );
-
-		switch($world_id){
-			case $data->worlds->red :
-				$this->home_color = "red";
-				break;
-			case $data->worlds->blue :
-				$this->home_color = "blue";
-				break;
-			case $data->worlds->green :
-				$this->home_color = "green";
-				break;
+		if(false === ( $score = get_transient( 'gw2_wvw_' . $this->current_world ) ) ){
+			$score = $this->get_gw_api( "https://api.guildwars2.com/v2/wvw/matches?world=" . $world_id );
+			set_transient( 'gw2_wvw_' . $this->current_world, $score, 600);
 		}
+
+		$score = $this->get_gw_api( "https://api.guildwars2.com/v2/wvw/matches?world=" . $world_id );
+
+		$team = array(
+			'red' => array(
+				'host' => $score->worlds->red,
+				'guests' => array_diff($score->all_worlds->red, array($score->worlds->red)),
+				'scores' => $score->scores->red,
+				'current_skirmish_scores' => end($score->skirmishes)->scores->red,
+				'kills' => $score->kills->red,
+				'deaths' => $score->deaths->red,
+				'v_scores' => $score->victory_points->red
+			),
+			'blue' => array(
+				'host' => $score->worlds->blue,
+				'guests' => array_diff($score->all_worlds->blue, array($score->worlds->blue)),
+				'scores' => $score->scores->blue,
+				'current_skirmish_scores' => end($score->skirmishes)->scores->blue,
+				'kills' => $score->kills->blue,
+				'deaths' => $score->deaths->blue,
+				'v_scores' => $score->victory_points->blue
+			),
+			'green' => array(
+				'host' => $score->worlds->green,
+				'guests' => array_diff($score->all_worlds->green, array($score->worlds->green)),
+				'scores' => $score->scores->green,
+				'current_skirmish_scores' => end($score->skirmishes)->scores->green,
+				'kills' => $score->kills->green,
+				'deaths' => $score->deaths->green,
+				'v_scores' => $score->victory_points->green
+			)
+		);
+
+		$guest_name = array(
+			'red' => '',
+			'green' => '',
+			'blue' => ''
+		);
+		$color = array('red', 'blue', 'green');
+		foreach($color as $single){
+			if(count($team[$single]['guests']) > 0){
+				foreach($team[$single]['guests'] as $team_guest){
+					$guest[$single][] = $this->worlds[$team_guest];
+				}
+				//$guest_name[$single] = "(".implode(", ", $guest[$single]).")";
+				$guest_name[$single] = implode(", ", $guest[$single]);
+			}
+			unset($guest);
+		}
+
+		if(in_array($world_id, $score->all_worlds->red)){
+			$this->home_color = "red";
+		}elseif(in_array($world_id, $score->all_worlds->green)){
+			$this->home_color = "green";
+		}elseif(in_array($world_id, $score->all_worlds->blue)){
+			$this->home_color = "blue";
+		}
+
+		$this->match_info = $team;
+		$this->linked = $guest_name;
 	}
 }
-
-$gw2wvw = new Gw2wvw();
-var_dump($gw2wvw);
 ?>
--- wp-gw2-wvw.php
+++ wp-gw2-wvw.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: Guild Wars 2 WvW Matchups
+Plugin Name: Guild Wars 2 World vs World
 Plugin URI:  https://github.com/IronGhost63/wp-gw2-wvw
 Description: WordPress Plugin for displaying Guild Wars 2 WvW Matchup information
 Version:     1.0
@@ -8,9 +8,90 @@ Author:      Jirayu Yingthawornsuk
 Author URI:  https://jirayu.in.th
 License:     GPL2
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
-Text Domain: gw2wvw
+Text Domain: gw2wvwinfo
 */
 
 include("gw2wvw.class.php");
 
+add_action("wp_enqueue_scripts", "mn_enqueue");
+add_shortcode("gw2wvw", "gw2wvw_shortcode");
+
+function mn_enqueue(){
+	wp_register_style( "gw2wvwinfo", plugins_url("wp-gw-style.css", __FILE__));
+	wp_enqueue_style( "gw2wvwinfo" );
+}
+
+function gw2wvw_shortcode($attr){
+	extract(shortcode_atts(array(
+		'world' => '1080',
+	), $atts));
+
+	$gw2 = new Gw2wvw(array('current_world' => $world));
+	$worlds = $gw2->worlds;
+
+	$vscores = array(
+		$gw2->match_info['red']['v_scores'],
+		$gw2->match_info['green']['v_scores'],
+		$gw2->match_info['blue']['v_scores'],
+	);
+	$cscores = array(
+		$gw2->match_info['red']['current_skirmish_scores'],
+		$gw2->match_info['green']['current_skirmish_scores'],
+		$gw2->match_info['blue']['current_skirmish_scores'],
+	);
+
+	rsort($vscores);
+	rsort($cscores);
+
+	$rows = "";
+	foreach(array('red', 'green', 'blue') as $single){
+		$server_name = '<span class="wvw-server-home">' . $worlds[$gw2->match_info[$single]['host']] . '</span>';
+
+		if($gw2->home_server === $gw2->match_info[$single]['host']){
+			$server_name .= ' <i class="wvw-home-icon></i>';
+		}
+
+		if(!empty($gw2->linked[$single])){
+			$server_name .= '<br><span class="wvw-server-linked">' . __("Linked:") . '</span> <span class="wvw-server-linked-name">' . $gw2->linked[$single] . '</span>';
+		}
+
+		$vscore_width = round( (100/$vscores[0])*$gw2->match_info[$single]['v_scores'], 0 );
+		$cscore_width = round( (100/$cscores[0])*$gw2->match_info[$single]['current_skirmish_scores'], 0 );
+		$kdr = round( ($gw2->match_info[$single]['kills']/$gw2->match_info[$single]['deaths']), 2);
+
+		$rows .= <<<EOT
+			<tr class="result-red">
+				<td>{$server_name}</td>
+				<td><p class="wvw-percent wvw-percent-{$single}" style="width: {$vscore_width}%;">{$gw2->match_info[$single]['v_scores']}</p></td>
+				<td><p class="wvw-percent wvw-percent-{$single}" style="width: {$cscore_width}%;">{$gw2->match_info[$single]['current_skirmish_scores']}</p></td>
+				<td><span class="wvw-kdr">{$kdr}</span></td>
+			</tr>
+EOT;
+	}
+
+	$txt = array(
+		'worlds' => __("Worlds"),
+		'vscore' => __("Victory Points"),
+		'cscore' => __("Current Skirmish"),
+		'kdr' => __("KDR")
+	);
+
+	$html = <<<EOT
+	<table class="gw2wvw-info-table">
+		<thead>
+			<tr>
+				<th>{$txt['worlds']}</th>
+				<th>{$txt['vscore']}</th>
+				<th>{$txt['cscore']}</th>
+				<th>{$txt['kdr']}</th>
+			</tr>
+		</thead>
+		<tbody>
+			{$rows}
+		</tbody>
+	</table>
+EOT;
+
+	return $html;
+}
 ?>
